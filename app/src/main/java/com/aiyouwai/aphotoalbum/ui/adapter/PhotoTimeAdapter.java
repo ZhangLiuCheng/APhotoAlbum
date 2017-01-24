8d304b3b70afcf0b6d60df6065d14db377507f28
zlc <zlc@wlaa.com.cn>
2017-01-24 16:00:47 +0800

    develop

--- app/src/main/java/com/aiyouwai/aphotoalbum/ui/adapter/PhotoTimeAdapter.java
+++ app/src/main/java/com/aiyouwai/aphotoalbum/ui/adapter/PhotoTimeAdapter.java
@@ -1,11 +1,13 @@
 package com.aiyouwai.aphotoalbum.ui.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.aiyouwai.aphotoalbum.R;
 import com.aiyouwai.aphotoalbum.model.entity.Photo;
@@ -15,6 +17,9 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 public class PhotoTimeAdapter extends RecyclerView.Adapter<PhotoTimeAdapter.Holder> implements View.OnClickListener {
+
+    private String[] colors =  {"#FF0000", "#00FF00", "#0000FF"};
+    private int[] icons = {R.drawable.c_red, R.drawable.c_green, R.drawable.c_blue};
 
     private Context context;
     private LayoutInflater inflater;
@@ -47,7 +52,7 @@ public class PhotoTimeAdapter extends RecyclerView.Adapter<PhotoTimeAdapter.Hold
     public void onBindViewHolder(Holder holder, int position) {
         holder.itemView.setTag(data.get(position));
         holder.itemView.setOnClickListener(this);
-        holder.setData(data.get(position));
+        holder.setData(position, data);
     }
 
     @Override
@@ -60,14 +65,31 @@ public class PhotoTimeAdapter extends RecyclerView.Adapter<PhotoTimeAdapter.Hold
 
     class Holder extends RecyclerView.ViewHolder {
 
+        private TextView date;
+        private ImageView icon;
         private ImageView image;
 
         public Holder(View itemView) {
             super(itemView);
+            date = (TextView) itemView.findViewById(R.id.date);
+            icon = (ImageView) itemView.findViewById(R.id.icon);
             image = (ImageView) itemView.findViewById(R.id.image);
         }
 
-        public void setData(Photo photo) {
+        public void setData(int position, List<Photo> data) {
+            Photo photo = data.get(position);
+            if (position <= 0) {
+                date.setVisibility(View.VISIBLE);
+            } else {
+                if (photo.date.equals(data.get(position - 1).date)) {
+                    date.setVisibility(View.INVISIBLE);
+                } else {
+                    date.setVisibility(View.VISIBLE);
+                }
+            }
+            date.setTextColor(Color.parseColor(colors[position % 3]));
+            date.setText(photo.date);
+            icon.setImageResource(icons[position % 3]);
             Picasso.with(context)
                     .load(photo.image)
                     .placeholder(R.drawable.default_loading)
